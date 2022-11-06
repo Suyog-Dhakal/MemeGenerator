123a443b65472ae054d2b7ad5a6a2b98e8dd46f7
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-11-06 21:08:36 +0545

    input field and button created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Meme from "./components/Meme";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Meme />
     </div>
   );
 }
+
+//5.6
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <form action="" className="form">
+        <input type="text" className="form-inputs" placeholder="Top-text" />
+        <input type="text" className="form-inputs" placeholder="Bottom-text" />
+        <button className="form-button">Get a new meme image</button>
+      </form>
+    </main>
+  );
+}
