5a934a3c058dfcd24a1b000825acf1ee5fd60305
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-11-09 12:50:41 +0545

    image generated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,27 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
-      <form action="" className="form">
+      <div className="form">
         <input type="text" className="form-inputs" placeholder="Top-text" />
         <input type="text" className="form-inputs" placeholder="Bottom-text" />
-        <button className="form-button">Get a new meme image</button>
-      </form>
+        <button className="form-button" onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </div>
+      <img src={memeImage} className="meme-image" />
     </main>
   );
 }
+
+//5.18
