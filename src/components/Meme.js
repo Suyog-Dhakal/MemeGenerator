bba2828bcc6dff17cde3a103de543f2f39046e05
Suyog-Dhakal <suyogdhakal8@gmail.com>
2022-11-25 13:49:47 +0545

    form update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -20,16 +20,42 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" className="form-inputs" placeholder="Top-text" />
-        <input type="text" className="form-inputs" placeholder="Bottom-text" />
+        <input
+          type="text"
+          className="form-inputs"
+          placeholder="Top-text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-inputs"
+          placeholder="Bottom-text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form-button" onClick={getMemeImage}>
           Get a new meme image
         </button>
       </div>
-      <img src={meme.randomImage} className="meme-image" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
